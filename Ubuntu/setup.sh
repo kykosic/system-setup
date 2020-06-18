21067299616e95989ffb48cf38296f8207422ac3
Kyle Kosic <kylekosic@gmail.com>
2020-06-18 14:52:24 -0400

    more libs"

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-sudo chown -R $(whoami) /usr/local
-sudo chown -R $(whoami) /opt
-
 sudo apt-get update
 sudo apt-get install --yes \
-    zip
+    zip \
+    make \
+    gcc \
+    g++ \
+    libssl-dev \
+    pkg-config
