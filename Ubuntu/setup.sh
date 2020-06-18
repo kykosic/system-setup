ad072e89cc237548d5e94fe0d0e776ef3d685780
Kyle Kosic <kylekosic@gmail.com>
2020-06-18 15:03:13 -0400

    docker install

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+echo 'Installing libs'
 sudo apt-get update
 sudo apt-get install --yes \
     zip \
@@ -8,4 +9,19 @@ sudo apt-get install --yes \
     gcc \
     g++ \
     libssl-dev \
-    pkg-config
+    pkg-config \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common
+
+echo 'Installing docker'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker $(whoami)
+
+echo 'Docker installed, you may need to reboot now'
