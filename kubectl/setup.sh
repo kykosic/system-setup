73616b3bc26f13b6c71cd7dfe6f792222566ea87
Kyle Kosic <kylekosic@gmail.com>
2020-07-14 10:57:34 -0400

    Update setup.sh

--- kubectl/setup.sh
+++ kubectl/setup.sh
@@ -4,11 +4,13 @@ set -e
 cwd=$(pwd)
 cd "$HOME"
 
-echo "Installing kubectl"
+VERSION="v1.16.10"
+echo "Installing kubectl version $VERSION"
+echo "Remember to check the version of your kubernetes cluster, as the client must be +-1 minor version of the cluster"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/darwin/amd64/kubectl"
 else
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubectl
 fi
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
